ab8da1089673e919237e1c5a36748e540cdf4477
Brian Stacy <bstacy@worldbank.org>
2019-11-12 10:28:50 -0500

    updates

---
+++ R/GDP_satellites.R
@@ -0,0 +1,88 @@
+#Purpose:  This is example code to produce a raster map of the GDP per Square km using World Bank raster map fro DECRG.
+# This could be useful for examining lack of resources for schools for the Global Education Policy Dashboard project
+#Author: Brian Stacy
+#Date: 11/12/2019
+
+#read in packages
+library(tidyverse)
+library(rgdal)
+library(raster)
+library(leaflet)
+
+#Data downloaded from Here:
+#https://datacatalog.worldbank.org/dataset/gross-domestic-product-2010 also see
+#https://preview.grid.unep.ch/index.php?preview=data&events=socec&evcat=1&lang=eng
+# In the distributed global GDP dataset sub-national GRP and national GDP data are allocated to 
+# 30 arc second (approximately 1km) grid cells in proportion to the population residing in that cell. 
+# The method also distinguishes between rural and urban population, assuming the latter to have a higher 
+# GDP per capita. Input data are from 1) a global time-series dataset of GDP, with subnational gross regional 
+# product (GRP) for 74 countries, compiled by the World Bank Development Economics Research Group (DECRG). 2) 
+# Gridded population projections for the year 2009, based on a population grid for the year 2005 provided by 
+# LandScanTM Global Population Database (Oak Ridge, TN: Oak Ridge National Laboratory). This dataset has been 
+# extrapolated to year 2010 by UNEP/GRID-Geneva. Unit is estimated value of production per cell, in thousand of 
+# constant 2000 USD. Cell level anomalies may occur due to poor alignment of multiple input data sources, and it 
+# is strongly recommended that users attempt to verify information, or consult original sources, in order to determine 
+# suitability for a particular application. This product was compiled by DECRG for the Global Assessment Report on Risk 
+# Reduction (GAR). It was modeled using global data. Credit: GIS processing World Bank DECRG, Washington, DC, 
+# extrapolation UNEP/GRID-Geneva.
+
+######################
+# Read in Data
+######################
+
+#file directory
+download_dir <- "C:/Users/wb469649/Documents/Github/GDP_satellite_maps/Data"
+
+
+#basic info on tiff file
+GDALinfo(paste(download_dir, "GDP.tif", sep="/"))
+
+#open the raster
+gdp_raster <- raster(paste(download_dir, "GDP.tif", sep="/"))
+summary(gdp_raster)
+
+#convert to dataframe for ggplot
+gdp_raster_df <- as.data.frame(gdp_raster, xy=TRUE)
+
+#create bins of GDP based on K-means clustering
+kmeans <- kmeans(gdp_raster_df$GDP, 100)
+
+gdp_raster_df$cluster <- as.factor(kmeans$cluster) 
+
+#create dataframe based on GDP centers from kmeans
+gdp_centers <- as.data.frame(kmeans$centers) %>%
+  mutate(cluster=as.factor(row.names(.))) %>%
+  rename(gdp_cluster=V1)
+
+#join the GDP kmeans data and drop GDP with zero (seems to usually be water areas or forest)
+gdp_raster_df <- gdp_raster_df %>%
+  left_join(gdp_centers) %>%
+  filter(GDP>0)
+
+#plot the GDP data
+gdp_plot <- ggplot() +
+  geom_raster(data=gdp_raster_df, aes(x=x, y=y, fill=as.numeric(cluster))) +
+  scale_fill_gradient2() +
+  coord_quickmap()
+
+gdp_plot
+
+
+###################
+#plot using leaflet
+###################
+#color palette
+values(gdp_raster)[values(gdp_raster)<=0]=NA
+
+#create a pallete with specified bins
+pal <- colorBin("RdYlBu", domain = c(0,100000), bins=c(0,5,10,20,30,40,50,100,10000000), 
+                    na.color = "transparent")
+
+
+gdp_leaflet <- leaflet() %>%
+  addTiles() %>%
+  addRasterImage(gdp_raster, colors=pal, opacity=0.8) %>%
+  addLegend(pal=pal, values=values(gdp_raster), title="GDP per Square km")
+
+gdp_leaflet
+
